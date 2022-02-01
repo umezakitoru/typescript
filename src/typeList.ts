b21e119d45b27276bb9b85ac9bf4c51a28f7d395
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-02-02 07:15:24 +0900

    111

---
+++ src/typeList.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** TypeScriptの基本の型 */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "aaa";
+
+// Array配列
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = () => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefind1: undefined = undefined;
+
+// Object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "aaa" };
