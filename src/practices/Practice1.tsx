dadc63e93951813583d33498d1956eb2029d9017
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-02-03 07:08:09 +0900

    111

---
+++ src/practices/Practice1.tsx
@@ -0,0 +1,9 @@
+export const Practice1 = () => {
+  const onClickPractice = () => alert();
+  return (
+    <div>
+      <p>練習問題：引数の型指定</p>
+      <button onClick={onClickPractice}>練習問題１を実行</button>
+    </div>
+  );
+};
